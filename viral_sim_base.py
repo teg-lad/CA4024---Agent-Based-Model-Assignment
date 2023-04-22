9a249dbdfd9b15b595d4abcc3b78b18deb7cc7af
Adam <adam.tegart2@mail.dcu.ie>
2023-04-22 23:35:06 +0100

    Fixed basic reproduction rate, added virus parameters and commented code

--- viral_sim_base.py
+++ viral_sim_base.py
@@ -48,17 +48,17 @@ params = {"max_infection_rate": 0.9,
 # Dictionary to store the virus attributes.
 virus_dict = {"covid": {"max_infection_rate": 0.9,
                         "case_fatality_rate": 0.025,
-                        "min_recovery_period": 14,
-                        "max_recovery_period": 31,
-                        "min_carrier_period": 1,
-                        "max_carrier_period": 5},
+                        "min_recovery_period": 11,
+                        "max_recovery_period": 20,
+                        "min_carrier_period": 4,
+                        "max_carrier_period": 6},
 
-              "other": {"max_infection_rate": 0.9,
-                        "case_fatality_rate": 0.025,
+              "marburg": {"max_infection_rate": 0.9,
+                        "case_fatality_rate": 0.88,
                         "min_recovery_period": 14,
                         "max_recovery_period": 31,
-                        "min_carrier_period": 1,
-                        "max_carrier_period": 5}}
+                        "min_carrier_period": 2,
+                        "max_carrier_period": 21}}
 
 # Prompt the user for the name of the virus, if none is supplied the default parameters are used.
 virus_name = input(
@@ -113,9 +113,13 @@ cd = 0.05  # neighborhood radius
 output_path = Path.cwd() / "Simulations" / f"{virus_name}_{timestamp}"
 output_path.mkdir(parents=True, exist_ok=True)
 
+# List to capture the cases from deceased agents.
+deceased_cases = []
+
 
 class agent:
     pass
+
 
 # For each of the vaccination rates, run the experiments.
 # Note: the simulation stops updating but does not stop running as pycxsimulator keeps calling the functions.
@@ -149,7 +153,6 @@ for vac_rate in vac_rates:
 
             # start with just 1 infected agent.
             if i >= pop_init - infected_init:
-
                 # Give the agent type infected, an attribute to store the time infected and a randomly sampled
                 # recovery time from a uniform distribution between a minimum and maximum.
                 ag.type = 'infected'
@@ -200,7 +203,7 @@ for vac_rate in vac_rates:
         global infected_ts, total_infected_ts, casualty_ts, total_casualty_ts, basic_reproduction_number_ts
 
         # Create a subplot for displaying the agents in the environment.
-        subplot(3, 1, (1,2))
+        subplot(3, 1, (1, 2))
 
         plt.show()
 
@@ -232,18 +235,24 @@ for vac_rate in vac_rates:
         tight_layout()
         title('Total casualties')
 
+        # Save the figure for use during the write-up.
         savefig(agents_path / f"{time}_agents.png")
 
 
     def update():
+        """
+        This function updates the agents asynchronously.
+        """
         global agents, time, current_output_path
         global daily_infected, daily_casualties
         global total_infected, total_casualties
         global infected_ts, total_infected_ts, casualty_ts, total_casualty_ts, basic_reproduction_number_ts
 
-        # if there are no agents left, end
+        # if there are no agents left, end the simulation.
+        # Note: this just prevents future updates, pycxsimulator continues to call the functions.
         carrier = [ag for ag in agents if ag.type == 'carrier']
         infected = [ag for ag in agents if ag.type == 'infected']
+
         if not carrier and not infected:
             pkl_file = current_output_path / "statistics.pkl"
             if not pkl_file.exists():
@@ -260,7 +269,7 @@ for vac_rate in vac_rates:
         # randomly choose an agent to move (asynchronous updating)
         ag = choice(agents)
 
-        # simulate random movement before reproduction behaviour
+        # simulate random movement before agent interactions
         ag.x += uniform(-speed, speed)
         ag.x = clip(ag.x, 0, 1)
         ag.y += uniform(-speed, speed)
@@ -269,48 +278,62 @@ for vac_rate in vac_rates:
         # susceptible behaviour
         if ag.type == 'susceptible':
 
+            # Get the distances to the infected agents.
             squared_distance_to_infected = {nb: (((ag.x - nb.x) ** 2) + (ag.y - nb.y) ** 2) for nb in agents if
                                             nb.type == 'infected'}
 
+            # If we have more than one infected.
             if len(squared_distance_to_infected) > 0:
 
-                # if there is infected agent nearby, some probability of getting infected
+                # Get the closest agent and get the distance from the current agent to this neighbour.
                 closest_nb = min(squared_distance_to_infected, key=squared_distance_to_infected.get)
                 distance = math.sqrt(squared_distance_to_infected[closest_nb])
 
+                # Scale the infection rate dependent on the distance between the agents.
                 infection_rate = max(0, (max_infection_rate * (1 - (distance / cd))))
 
+                # If the agent is immune (vaccinated or recovered) lower the chance of infection.
                 if ag.immune:
                     infection_rate = infection_rate / 5
 
+                # If the neighbour is immune (vaccinated or recovered) lower the chance of infection. The motivation is
+                # that an immune agent will have a lower viral load, meaning the virus is not present in large amounts.
                 if closest_nb.immune:
                     infection_rate = infection_rate / 5
 
+                # If the infection rate is non-zero.
                 if infection_rate > 0:
 
+                    # Check if the agent has got infected.
                     if random() < infection_rate:
+
+                        # If this agent was not previously infected, add them to the neighbours secondary infections for
+                        # the basic reproduction number
                         if not ag.prior_infection:
                             ag.prior_infection = True
                             ag.new_infection = True
-                            ag.basic_reproductive_time = 2
                             closest_nb.no_infected += 1
 
+                        # Change the agent to a carrier type
                         ag.type = 'carrier'
 
+                        # Set up the agent with a recovery time, carrier time and infected time.
                         ag.rec_time = uniform(rec_time_range[0], rec_time_range[1])
                         ag.carrier_time = uniform(carrier_time_range[0], carrier_time_range[1])
                         ag.infected_time = 0
+
+                        # Add this infection to the metrics.
                         daily_infected += 1
                         total_infected += 1
 
         # carrier behaviour
         if ag.type == 'carrier':
 
-            # agent becomes immune after a week if they survive
+            # After the incubation period the carrier becomes an infected.
             if ag.carrier_time < ag.infected_time:
                 ag.type = 'infected'
             else:
-                # if they survive, update the amount of time the agent was infected
+                # Otherwise, increase the time by one unit.
                 ag.infected_time += 1
 
         # infected behaviour
@@ -330,37 +353,49 @@ for vac_rate in vac_rates:
 
             random_death = random()
 
+            # If the agent is immune they are less likely to die from the infection.
             if ag.immune:
                 dynamic_prob_death = dynamic_prob_death / 10
 
+            # Check if the agent has died.
             if random_death < dynamic_prob_death:
-                print("Agent died with prob:", dynamic_prob_death)
+                # If they have, add their secondary cases to the list to allow them to be used in the calculation of the
+                # basic reproduction number metric.
+                deceased_cases.append(ag.no_infected)
                 agents.remove(ag)
+
+                # Add the casualty to the metrics.
                 total_casualties += 1
                 daily_casualties += 1
 
-            # agent becomes immune after a week if they survive
+            # agent recovers and becomes immune if they survive until recovery.
             elif ag.rec_time < ag.infected_time:
                 ag.immune = True
                 ag.type = "susceptible"
+
+            # If they survuve, update the infected time for this agent.
             else:
-                # if they survive, update the amount of time the agent was infected
                 ag.infected_time += 1
-        # immune behaviour - assume immune agents don't die or get infected
 
 
     def update_one_unit_time():
-        ''' Each "update" should result in each agent moving an average of 1 time. '''
+        """
+        Each "update" should result in each agent moving an average of 1 time.
+        """
         global agents, time
         global daily_infected, daily_casualties
         global total_infected, total_casualties
         global infected_ts, total_infected_ts, casualty_ts, total_casualty_ts, basic_reproduction_number_ts
 
+        # Increase the number of time steps that have been performed.
         time += 1
 
+        # Initialise variables to store the daily infected and daily casualties.
         daily_infected = 0
         daily_casualties = 0
-        # variable to store the number of infections/casualties in a one unit-time update
+
+        # Perform an update for every agent.
+        # Note: The agents updated are chosen randomly, so an agent may not be updated in a time step.
         t = 0
         while t < 1:
             t += 1 / len(agents)
@@ -372,44 +407,71 @@ for vac_rate in vac_rates:
         total_infected_ts.append(total_infected)
         total_casualty_ts.append(total_casualties)
 
-        basic_reproduction_number_ts.append(np.mean([ag.no_infected for ag in agents if ag.new_infection]))
+        # Get the number of secondary cases. These are the cases that have been confirmed, excluding new infections from
+        # the current time step.
+        secondary_cases = [ag.no_infected for ag in agents if ag.type == "infected" and not ag.new_infection] + deceased_cases
+
+        # If we have seconday cases, we can average them.
+        if len(secondary_cases) > 0:
+            basic_reproduction_number_ts.append(np.mean(secondary_cases))
+        # Otherwise, append 0.
+        else:
+            basic_reproduction_number_ts.append(0)
 
         # then reset it for the next time step
         for ag in agents:
-            ag.no_infected = 0
             ag.new_infection = False
 
+
     def max_infection_rate_param(val=max_infection_rate):
+        """
+        This function allow the max_infection_rate to be changed within the simulation GUI.
+        """
         global max_infection_rate
         max_infection_rate = float(val)
         return max_infection_rate
 
 
     def case_fatality_rate_param(val=case_fatality_rate):
+        """
+        This function allow the case_fatality_rate to be changed within the simulation GUI.
+        """
         global case_fatality_rate
         case_fatality_rate = float(val)
         return case_fatality_rate
 
 
     def min_recovery_period_param(val=min_recovery_period):
+        """
+        This function allow the min_recovery_period to be changed within the simulation GUI.
+        """
         global min_recovery_period
         min_recovery_period = int(val)
         return min_recovery_period
 
 
     def max_recovery_period_param(val=max_recovery_period):
+        """
+        This function allow the max_recovery_period to be changed within the simulation GUI.
+        """
         global max_recovery_period
         max_recovery_period = int(val)
         return max_recovery_period
 
 
     def min_carrier_period_param(val=min_carrier_period):
+        """
+        This function allow the min_carrier_period to be changed within the simulation GUI.
+        """
         global min_carrier_period
         min_carrier_period = int(val)
         return min_carrier_period
 
 
     def max_carrier_period_param(val=max_carrier_period):
+        """
+        This function allow the max_carrier_period to be changed within the simulation GUI.
+        """
         global max_carrier_period
         max_carrier_period = int(val)
         return max_carrier_period
@@ -418,9 +480,3 @@ for vac_rate in vac_rates:
     pycxsimulator.GUI(parameterSetters=[max_infection_rate_param, case_fatality_rate_param, min_recovery_period_param,
                                         max_recovery_period_param, min_carrier_period_param, max_carrier_period_param]
                       ).start(func=[initialize, observe, update_one_unit_time])
-
-    # plot(infected_ts, color='green')
-    # title('Daily infections')
-    # show()
-
-    print("Total casualties: {}".format(total_casualties))
